bc38f806b4fdfbbdbe368dbb2408671921be7510
DeepanshUnravel <deepansh@gounravel.com>
2026-01-11 23:25:54 +0530

    Implemented the authorization middleware.

--- Server/src/middlewares/authorization.js
+++ Server/src/middlewares/authorization.js
@@ -1,16 +1,13 @@
-import { validateAdminUser } from "../services/authorization.service.js";
 import AppError from "../utils/AppError.js";
 
 export default async function authorizationMiddleware(req, res, next) {
     try {
-        // const authorize = await validateAdminUser(req.user);
+        const user = req.user;
 
-        if (user?.role !== "admin") {
-                AppError("Access denied. Admin role required.", 403);
-           };
+        if (!user?.role || user?.role !== "admin") {
+            AppError("Access denied. Admin role required.", 403);
+        };
 
-
-        console.log("NEXT WILL RUN HERE");
         next();
 
     } catch (err) {
