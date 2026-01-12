b873ee123ae9f536f544d1c3dea91804bb6b7128
DeepanshUnravel <deepansh@gounravel.com>
2026-01-12 17:29:39 +0530

    Added the function to calculate the GST rates and discount.

--- Server/src/utils/product.utils.js
+++ Server/src/utils/product.utils.js
@@ -14,22 +14,29 @@ export const calculatePrices = (product) => {
     const { price, discountPrice, taxCategory } = product;
 
     const gstRate = GST_RATES[taxCategory];
+    // 0.05
 
     const baseOriginalPrice = price;
+    // 5799
 
     const baseDiscountedPrice = discountPrice ?? price;
+    // 4999
 
     const originalTaxAmount = Math.ceil(baseOriginalPrice * gstRate);
+    // 5799 * 0.05 = 289
+
     const discountedTaxAmount = Math.ceil(baseDiscountedPrice * gstRate);
+    // 4999 * 0.05 = 250
 
     const originalFinalPrice = Math.ceil(baseOriginalPrice + originalTaxAmount);
+    // 5799 + 289 = 6088
     const discountedFinalPrice = Math.ceil(baseDiscountedPrice + discountedTaxAmount);
-
+    // 4999 + 250 =5249
     const discountAmount = baseOriginalPrice - baseDiscountedPrice;
 
     const discountPercent =
         discountAmount > 0
-            ? Math.ceil((discountAmount / baseOriginalPrice) * 100)
+            ? Number((discountAmount / baseOriginalPrice) * 100).toFixed(2)
             : 0;
     return {
         discountPercent,
