d6c762bde3d0930de2f9e7603b5fadd69da325a6
DeepanshUnravel <deepansh@gounravel.com>
2026-01-15 23:51:04 +0530

    Add logo and enhance navbar layout: include logo image, improve styling, and integrate cart icon

--- Client/src/shared/components/navbar/Navbar.jsx
+++ Client/src/shared/components/navbar/Navbar.jsx
@@ -1,36 +1,45 @@
 // src/shared/components/Navbar/Navbar.jsx
 import { useState } from "react";
 import "./Navbar.css";
-import { GiHamburgerMenu, IoMdClose } from "../../icons/icons.js";
+import { GiHamburgerMenu, IoMdClose, BsCart2 } from "../../icons/icons.js";
+import { ritsaRugsLogo } from "../../images/images.js";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const navigate = useNavigate();
 
     return (
         <nav className="navbar">
+            <div className="nc">
 
-            <button
-                className="navbar__menu-btn"
-                onClick={() => setIsOpen(true)}
-                aria-label="Toggle menu"
-            >
-                <GiHamburgerMenu />
-            </button>
+                <div
+                    className="navbar__menu-btn"
+                    onClick={() => setIsOpen(true)}
+                >
+                    <GiHamburgerMenu />
+                </div>
 
-            <div className="navbar__container">
-                <div className="navbar__logo">LOGO</div>
+                <div className="navbar__container">
+                    <div className="navbar__logo" onClick={()=>navigate("/")}>
+                        <img src={ritsaRugsLogo} alt="Ritsa Rugs Logo" />
+                    </div>
 
+                </div>
+
+                <div className="">
+                    <BsCart2 />
+                </div>
             </div>
 
 
             <aside className={`navbar__menu__container ${isOpen && "show_profile"} `}>
-                <button
+                <div
                     className="navbar__menu-btn close_menu_button"
                     onClick={() => setIsOpen(false)}
-                    aria-label="Toggle menu"
                 >
                     <IoMdClose />
-                </button>
+                </div>
                 <ul className="navbar__menu">
                     <li>Home</li>
                     <li>Rugs</li>
--- Client/src/shared/icons/icons.js
+++ Client/src/shared/icons/icons.js
@@ -4,7 +4,14 @@ import { GiHamburgerMenu } from "react-icons/gi";
 // close icon
 import { IoMdClose } from "react-icons/io";
 
+// cart icon
+import { BsCart2 } from "react-icons/bs";
+
+
+
+
 export {
     GiHamburgerMenu,
-    IoMdClose
+    IoMdClose,
+    BsCart2
 };
