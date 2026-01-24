7cada10cf737e220c426635708e98f9823374bc6
DeepanshUnravel <deepansh@gounravel.com>
2026-01-24 23:52:10 +0530

    Added the home page carousel

---
+++ Client/src/shared/config/carouselConfig.js
@@ -0,0 +1,29 @@
+import { carousel1, carousel2, carousel3 } from "../images/images"
+
+export const carouselConfig = [
+    {
+        id: 1,
+        src: carousel1,
+        alt: "carousel image 1",
+    },
+    {
+        id: 2,
+        src: carousel2,
+        alt: "carousel image 2",
+    },
+    {
+        id: 3,
+        src: carousel3,
+        alt: "carousel image 3",
+    },
+    // {
+    //     id: 4,
+    //     src: carousel4,
+    //     alt: "carousel image 4",
+    // },
+    // {
+    //     id: 5,
+    //     src: carousel5,
+    //     alt: "carousel image 5",
+    // },
+];
--- Client/src/shared/images/images.js
+++ Client/src/shared/images/images.js
@@ -2,7 +2,15 @@ import ritsaRugsLogo from "../../assets/images/ritsa-rugs.png"
 // import ritsaRugsLogo from "../../assets/images/Ritsa Rugs.png"
 import Google from "../../assets/images/google.png"
 
+import carousel1 from "../../assets/images/carousel_image_1.jpg"
+import carousel2 from "../../assets/images/carousel_image_2.jpg"
+import carousel3 from "../../assets/images/carousel_image_3.jpg"
+
+
 export {
     ritsaRugsLogo,
     Google,
-}
+    carousel1,
+    carousel2,
+    carousel3,
+};
