2ef6afd7b89ef03a25afa8812bd248057329a844
DeepanshUnravel <deepansh@gounravel.com>
2026-01-24 16:14:43 +0530

    changed the logo of ritsa rugs

--- Client/src/shared/images/images.js
+++ Client/src/shared/images/images.js
@@ -1,4 +1,5 @@
-import ritsaRugsLogo from "../../assets/images/Ritsa Rugs.png"
+import ritsaRugsLogo from "../../assets/images/ritsa-rugs.png"
+// import ritsaRugsLogo from "../../assets/images/Ritsa Rugs.png"
 import Google from "../../assets/images/google.png"
 
 export {
