8bff0e8718086de89c8f16b2898258054c14a8e3
DeepanshUnravel <deepansh@gounravel.com>
2026-01-24 22:51:15 +0530

    .

--- Client/src/features/home/pages/Home.jsx
+++ Client/src/features/home/pages/Home.jsx
@@ -1,7 +1,13 @@
+import { useSelector } from "react-redux";
 import { API_PATHS } from "../../../services/apiPaths";
 import SwiperCarousel from "../../../shared/components/carousel/Swiper.jsx";
 
 const Home = () => {
+
+    const token = useSelector((state) => state.auth.accessToken);
+
+    console.log("ACCESS TOKEN IS - ", token);
+
     return (
         <section>
             <SwiperCarousel />
